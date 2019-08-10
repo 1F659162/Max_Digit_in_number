e1d468783c366b551320af5919f535a865241bf7
Kaittrakul jaroenpong 6206021620159 IT1RA <45314100+1F659162@users.noreply.github.com>
2019-08-10 22:02:34 +0700

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+	int number ;
+	int max = 0 ;
+	do
+	{
+		cout << "Enter number : ";
+		cin >> number;
+		if (number == 0) break;
+		else
+		{
+			while(number != 0)
+			{
+				if((number % 10) > max) max = number %10;
+				number = number / 10 ;
+			}
+			cout << "Maximum of number is : " << max << endl << endl;
+		}
+		max = 0;
+	}while(true) ;
+	system("pause");
+	return(0);
+}
